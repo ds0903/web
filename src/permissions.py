a675e13e585b7e16f570e7f7976dd2cd3b8f6123
ds0903 <danilgrey15@gmail.com>
2024-04-25 13:17:06 +0300

    hot fix

--- src/permissions.py
+++ src/permissions.py
@@ -8,7 +8,7 @@ class IsADMIN(permissions.BasePermission):
         if user.role == "admin" and request.method == "DELETE":
             return True
         if (
-            user.role == "admin" or user.role == "senior" and request.method == "PUT"
+            user.role == "admin" or user.role == "senior" and request.method == "PUT" # noqa
         ):  # noqa
             # return request.user.is_staff
             return True
